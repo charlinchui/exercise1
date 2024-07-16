1ca7e1eb5632074387f3294fd67c7b89c0179bce
Carlos Bueno Coig-O'Donnel <cbuenocoig-odonnel@deloitte.ie>
2024-07-16 16:56:01 +0100

    Revert "Error handling fix"
    This reverts commit e3e0acae1ac10c56bcdc640d2edc5c72b9464db0.

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -2,13 +2,9 @@ package main
 
 import (
 	"charlinchui/SomeTimeInc/internal/router"
-	"log"
 	"net/http"
 )
 
 func main() {
-	log.Println("Server starting on port 8080")
-	if err := http.ListenAndServe(":8080", router.Router()); err != nil {
-		log.Fatal("ERROR STARTING SERVER")
-	}
+	http.ListenAndServe(":8080", router.Router())
 }
