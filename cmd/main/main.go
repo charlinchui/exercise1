ff19794224d9eb0591e4b4caa5a934530d2a48fc
Carlos Bueno Coig-O'Donnel <cbuenocoig-odonnel@deloitte.ie>
2024-07-16 16:58:27 +0100

    Error handling fix

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -2,9 +2,13 @@ package main
 
 import (
 	"charlinchui/SomeTimeInc/internal/router"
+	"log"
 	"net/http"
 )
 
 func main() {
-	http.ListenAndServe(":8080", router.Router())
+	log.Println("SERVER STARTING ON PORT 8080")
+	if err := http.ListenAndServe(":8080", router.Router()); err != nil {
+		log.Fatal("ERROR STARTING SERVER")
+	}
 }
